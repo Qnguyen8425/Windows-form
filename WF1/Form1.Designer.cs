cb8cf131996b2f39ec52e257790e3181c34ba343
QuangNguyen8405 <161407117+QuangNguyen8405@users.noreply.github.com>
2024-12-20 23:54:07 +0700

    first times

---
+++ WF1/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace WF1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.a = new System.Windows.Forms.TextBox();
+            this.kq = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.b = new System.Windows.Forms.TextBox();
+            this.c = new System.Windows.Forms.TextBox();
+            this.Nghiem1 = new System.Windows.Forms.Label();
+            this.Nghiem2 = new System.Windows.Forms.Label();
+            this.Nghiem3 = new System.Windows.Forms.Label();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập a";
+            // 
+            // a
+            // 
+            this.a.Location = new System.Drawing.Point(177, 71);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(100, 22);
+            this.a.TabIndex = 1;
+            // 
+            // kq
+            // 
+            this.kq.Location = new System.Drawing.Point(362, 132);
+            this.kq.Name = "kq";
+            this.kq.Size = new System.Drawing.Size(75, 23);
+            this.kq.TabIndex = 2;
+            this.kq.Text = "Kết quả";
+            this.kq.UseVisualStyleBackColor = true;
+            this.kq.Click += new System.EventHandler(this.kq_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(346, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nhập b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(531, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nhập c";
+            // 
+            // b
+            // 
+            this.b.Location = new System.Drawing.Point(349, 71);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(100, 22);
+            this.b.TabIndex = 5;
+            // 
+            // c
+            // 
+            this.c.Location = new System.Drawing.Point(534, 71);
+            this.c.Name = "c";
+            this.c.Size = new System.Drawing.Size(100, 22);
+            this.c.TabIndex = 6;
+            // 
+            // Nghiem1
+            // 
+            this.Nghiem1.AutoSize = true;
+            this.Nghiem1.Location = new System.Drawing.Point(346, 189);
+            this.Nghiem1.Name = "Nghiem1";
+            this.Nghiem1.Size = new System.Drawing.Size(0, 16);
+            this.Nghiem1.TabIndex = 7;
+            // 
+            // Nghiem2
+            // 
+            this.Nghiem2.AutoSize = true;
+            this.Nghiem2.Location = new System.Drawing.Point(346, 241);
+            this.Nghiem2.Name = "Nghiem2";
+            this.Nghiem2.Size = new System.Drawing.Size(0, 16);
+            this.Nghiem2.TabIndex = 8;
+            // 
+            // Nghiem3
+            // 
+            this.Nghiem3.AutoSize = true;
+            this.Nghiem3.Location = new System.Drawing.Point(346, 216);
+            this.Nghiem3.Name = "Nghiem3";
+            this.Nghiem3.Size = new System.Drawing.Size(0, 16);
+            this.Nghiem3.TabIndex = 9;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(177, 132);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 10;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(547, 132);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 11;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(811, 512);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Nghiem3);
+            this.Controls.Add(this.Nghiem2);
+            this.Controls.Add(this.Nghiem1);
+            this.Controls.Add(this.c);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.kq);
+            this.Controls.Add(this.a);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox a;
+        private System.Windows.Forms.Button kq;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox b;
+        private System.Windows.Forms.TextBox c;
+        private System.Windows.Forms.Label Nghiem1;
+        private System.Windows.Forms.Label Nghiem2;
+        private System.Windows.Forms.Label Nghiem3;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Close;
+    }
+}
+
---
+++ WF1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void PTB2(int a, int b, int c)
+        {
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    if (c == 0)
+                    {
+                        Nghiem3.Text = "Phương trình vô số nghiệm";
+                    }
+                    else
+                    {
+                        Nghiem3.Text = "Phương trình vô nghiệm";
+                    }
+                }
+                else
+                {
+                    double x = -c / (double)b;
+                    Nghiem3.Text = $"x = {x:F2}";
+                }
+            }
+            else
+            {
+                double delta = b * b - 4 * a * c;
+                if (delta > 0)
+                {
+                    double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    Nghiem1.Text = $"x1 = {x1:F2}";
+                    Nghiem2.Text = $"x2 = {x2:F2}";
+                }
+                else if (delta == 0)
+                {
+                    double x = -b / (2 * a);
+                    Nghiem3.Text = $"x = {x:F2}";
+                }
+                else
+                {
+                    Nghiem3.Text = "Phương trình vô nghiệm";
+                }
+            }
+        }
+
+        private void kq_Click(object sender, EventArgs e)
+        {
+            int a1 = Convert.ToInt32(a.Text);
+            int b1 = Convert.ToInt32(b.Text);
+            int c1 = Convert.ToInt32(c.Text);
+            PTB2(a1, b1, c1);
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            a.Clear();
+            b.Clear();
+            c.Clear();
+            Nghiem1.Text = "";
+            Nghiem2.Text = "";
+            Nghiem3.Text = "";
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
